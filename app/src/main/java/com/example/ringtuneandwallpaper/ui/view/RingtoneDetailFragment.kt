a5b0b5d605abf159739c0453d582612408e5a94f
kynam <kynamnguyen2001@gmail.com>
2024-09-27 16:19:13 +0700

    Update: set device wallpaper from image url

--- app/src/main/java/com/example/ringtuneandwallpaper/ui/view/RingtoneDetailFragment.kt
+++ app/src/main/java/com/example/ringtuneandwallpaper/ui/view/RingtoneDetailFragment.kt
@@ -1,6 +1,14 @@
 package com.example.ringtuneandwallpaper.ui.view
 
+import android.app.AlarmManager
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.AlarmClock
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +16,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.ringtuneandwallpaper.databinding.FragmentRingtoneDetailBinding
+import java.io.File
 
 class RingtoneDetailFragment: Fragment(){
 
@@ -34,5 +43,42 @@ class RingtoneDetailFragment: Fragment(){
             findNavController().navigate(action)
         }
         binding.ringtoneName.text = listRing[position].name
+        binding.setRingtoneButton.setOnClickListener {
+//            val file = File(requireContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), listRing[position].name)
+//            val uri = saveFileToAlarmsDirectory(file)
+//            setAlarmSound(uri)
+        }
+    }
+
+    private fun saveFileToAlarmsDirectory(file: File): Uri {
+        val values = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, file.name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp3")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_ALARMS)
+        }
+
+        val resolver = requireContext().contentResolver
+        val uri = resolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values)
+
+        resolver.openOutputStream(uri!!).use { outputStream ->
+            file.inputStream().use { inputStream ->
+                inputStream.copyTo(outputStream!!)
+            }
+        }
+
+        return uri
+    }
+
+    private fun setAlarmSound(alarmUri: Uri) {
+        val alarmManager = requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_RINGTONE, alarmUri.toString())
+            putExtra(AlarmClock.EXTRA_MESSAGE, "Wake Up!")
+            putExtra(AlarmClock.EXTRA_HOUR, 7)
+            putExtra(AlarmClock.EXTRA_MINUTES, 30)
+        }
+        if (intent.resolveActivity(requireContext().packageManager) != null) {
+            requireContext().startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/ringtuneandwallpaper/ui/view/WallpaperDetailFragment.kt
+++ app/src/main/java/com/example/ringtuneandwallpaper/ui/view/WallpaperDetailFragment.kt
@@ -1,14 +1,28 @@
 package com.example.ringtuneandwallpaper.ui.view
 
+import android.annotation.SuppressLint
+import android.app.WallpaperManager
+import android.database.Cursor
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.ringtuneandwallpaper.databinding.FragmentWallpaperDetailBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.IOException
+import java.net.URL
 
 class WallpaperDetailFragment: Fragment(){
 
@@ -35,5 +49,11 @@ class WallpaperDetailFragment: Fragment(){
             findNavController().navigate(action)
         }
         binding.imageName.text = listWall[position].name
+        binding.setWallpaperButton.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                val inputStream = URL(listWall[position].url).openStream()
+                WallpaperManager.getInstance(requireContext()).setStream(inputStream)
+            }
+        }
     }
 }
